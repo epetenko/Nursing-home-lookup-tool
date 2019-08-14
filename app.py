a5f31beebfd05070374f6b82e3b429ef2726f9f9
Erin Petenko <erin.petenko@gmail.com>
2019-08-14 17:17:21 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template, jsonify
 app = Flask(__name__)
 
+# original pull of csv to create app
 def get_csv():
     csv_path = './static/Provider_info_w_cost_report_data.csv'
     csv_file = open(csv_path, 'r')
@@ -10,14 +11,14 @@ def get_csv():
     csv_list = list(csv_obj)
     return csv_list
 
-
+# Creates index.html template with csv data
 @app.route("/")
 def index():
     template = 'index.html'
     object_list = get_csv()
     return render_template(template, object_list=object_list)
 
-
+# creates detail page for each Nursing home
 @app.route('/<row_id>/')
 def detail(row_id):
     template = 'detail.html'
